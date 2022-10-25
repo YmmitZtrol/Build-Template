24ebf8db99b706fbb52fef5e52ede8444fae565f
YmmitZtrol <timwanghk@gmail.com>
2022-10-25 13:08:46 +0800

    Updated build to allow for running the program

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
+:: Arguments are:
+:: 				BUILD, DEBUG, RELEASE 	[Build Type - Default: DEBUG]
+::				run 					[Optional to run the programming after building]
+:: Example: build Release run
+:: Example: build run (build in DEBUG mode)
+
 @ECHO OFF
 ECHO ----------------------------------------------------------------------------------------------------
 ECHO ^|                       ----- Running Premake and creating Cmake Files -----                       ^|
@@ -9,6 +15,8 @@ ECHO ---------------------------------------------------------------------------
 cmake -S . -B ./build
 if [%1]==[] goto Default
 if [%1]=="run" goto Run
+if [%2]=="run" goto SpecificRun
+
 cmake --build ./build --config %1 
 
 :Default
@@ -17,3 +25,7 @@ cmake --build ./build --config DEBUG
 :Run
 cmake --build ./build --config DEBUG
 .\run
+
+:SpecificRun
+cmake --build ./build --config %1
+.\run %1
